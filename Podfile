91f7254999ef84db6b8f2a7e8b85deabe5a7dbcd
Pasco <pascocoder@gmail.com>
2016-03-05 15:12:51 +0800

    project init

---
+++ Podfile
@@ -0,0 +1,15 @@
+source 'https://gitcafe.com/akuandev/Specs.git'
+
+platform :ios, '7.0'
+
+target 'XidianInfo' do
+
+pod 'AFNetworking'
+pod 'Masonry'
+pod 'YYModel'
+pod 'SVProgressHUD'
+pod 'MJRefresh'
+pod 'ReactiveCocoa'
+
+
+end
---
+++ XidianInfo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XidianInfo
+//
+//  Created by Pasco on 16/3/5.
+//  Copyright © 2016年 Pasco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
